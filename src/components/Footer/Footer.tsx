6bcd81e1a1341317be8b681e13001a123389c11c
Michael <machapurin@icloud.com>
2024-01-13 02:46:49 +0300

    added link to github

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export function Footer() {
   return (
     <footer className={styles.footer}>
-        <a href="/">To github</a>
+        <a href="/https://machapurin.github.io/fake-store/">To github</a>
     </footer>
   )
 }
